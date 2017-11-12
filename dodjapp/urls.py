cad6063919927ba4c3b1b67c9772ea481a68e637
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-11-12 15:27:11 +0530

    Updated urls and views

--- dodjapp/urls.py
+++ dodjapp/urls.py
@@ -13,16 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import routers
 from rest_framework.authtoken import views as rest_framework_views
 
+from imgapp import views
 
+
+router = routers.SimpleRouter()
+router.register('image', views.ImageView, base_name='image') 
 urlpatterns = [
+	url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
     url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- imgapp/views.py
+++ imgapp/views.py
@@ -6,12 +6,25 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+
+from imgapp.models import Image
+from imgapp.serializers import ImageSerializer
 # Create your views here.
 
 
-class Image(APIView):
+class ImageView(APIView):
 	"""
 	CRUD for Images
 	"""
-	authentication_classes = ()
-	permission_classes = ()
+	model = Image
+	serializer_class = ImageSerializer
+
+	def get_queryset(self):
+		""" Return Image belonging to the current user"""
+		queryset = self.model.objects.all()
+		queryset = queryset.filter(user=self.request.user)
+		return queryset
+
+	def perform_create(self):
+		""" Associate current user as image owner """
+		return serializer.save(user=self.request.user)
