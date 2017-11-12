f3befa3b2413e4349bb2ddd64429bad1d26d88db
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-11-12 15:26:27 +0530

    Added user foreign key in image model

--- imgapp/models.py
+++ imgapp/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import uuid
 
+from django.contrib.auth.models import User
 from django.db import models
 
 from imgapp.utils import get_upload_file_path
@@ -10,6 +11,7 @@ from rest_framework.authtoken.models import Token
 
 # Create your models here.
 class Image(models.Model):
+	user = models.ForeignKey(User)
 	image = models.ImageField(upload_to=get_upload_file_path)
 	compressed_image = models.ImageField(upload_to=get_upload_file_path, blank=True, null=True)
 
