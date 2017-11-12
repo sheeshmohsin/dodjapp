a67ef89da85c6c2fcf9cc192734b00041f9ae730
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-11-12 15:26:44 +0530

    Added Image Serializer

---
+++ imgapp/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+from imgapp.models import Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+	user  = serializers.CharField(source='user.username',read_only=True)
+
+	class Meta:
+		model = Image
+		fields = ('user', 'image', 'compressed_image')
